40a15a138b185502746adc5e0ad29ee1d5bd3ad7
Ruxandra <ruxandra.arventiev@gmail.com>
2026-05-09 23:44:17 +0300

    .

--- src/strategy/classes/PlataPayPal.java
+++ src/strategy/classes/PlataPayPal.java
@@ -1,4 +1,8 @@
 package strategy.classes;
 
 public class PlataPayPal implements ModPlata{
+    @Override
+    public void plateste(String numeClient, double suma) {
+        System.out.println("Clientul "+ numeClient+" a platit cu PAYPAL suma de " + suma);
+    }
 }
